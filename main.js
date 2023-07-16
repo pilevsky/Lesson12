d808555bbf378b5719f639e9b0f248aec0a8d31e
pilevsky <pilevskiyd@yandex.ru>
2023-07-16 16:14:15 +0600

    Lesson12

--- main.js
+++ main.js
@@ -1,39 +1,28 @@
-let c = [];
-let d = new Set(c);
-d.add("e");
-d.add("r");
-d.add("i");
-d.add("k");
-d.add("t");
-d.add("h");
-d.add("e");
-d.add("b");
-d.add("e");
-d.add("s");
-d.add("t");
-console.log(d);
-
-let input = document.querySelector(".inp");
-let btn1 = document.querySelector(".btn1");
-let div = document.querySelector(".div");
-btn1.onclick = function () {
-  let inp = input.value;
-  console.log(d.has(inp));
-  if (d.has(inp) === false) {
-    d.add(inp);
-    console.log(d);
-    div.innerHTML = `<h2> True </h2>`;
-  } else {
-    div.innerHTML = `<h2> False </h2>`;
-  }
+let width = 50;
+let height = 50;
+document.querySelector(".one").onclick = function (event) {
+  console.log("d");
+  document.querySelector(".one").style.width = width + "px";
+  width++;
+  document.querySelector(".one").style.height = height + "px";
+  height++;
+};
+let hello = document.querySelector(".Hello").innerHTML;
+document.querySelector(".two").ondblclick = function (event) {
+  console.log(hello);
 };
 
-let a = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
-let b = [];
-let arr = Array.from(a);
-for (let key of arr) {
-  if (arr[key] >= 5) {
-    b.push(arr[key]);
+let img = document.querySelector(".img");
+img.onclick = function change() {
+  img.src =
+    "https://free-png.ru/wp-content/uploads/2021/06/free-png.ru-38-340x340.png";
+};
+
+function check(inp) {
+  if (typeof inp === "number") {
+    return console.log("False");
+  } else {
+    console.log("True");
   }
 }
-console.log(b);
+console.log(check(123));
